aca322a2879c45228c31f1ed4f4bfc16d23fbee9
mGalarnyk <mgalarny@gmail.com>
2017-01-10 21:08:27 -0800

    Added python linear regression .py file and made a better linear regression .ipynb file

--- Python_Basics/Linear_Regression/linearRegression.py
+++ Python_Basics/Linear_Regression/linearRegression.py
@@ -3,28 +3,39 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
+# Read in csv file
+# File: https://github.com/mGalarnyk/Python_Tutorials/blob/master/Python_Basics/Linear_Regression/linear.csv
 raw_data = pd.read_csv("linear.csv")
 
 # Removes rows with NaN in them
-
 filtered_data = raw_data[~np.isnan(raw_data["y"])] 
 
+x_y = np.array(filtered_data)
+x, y = x_y[:,0], x_y[:,1]
 
+# Reshaping
+x, y = x.reshape(-1,1), y.reshape(-1, 1)
 
-npMatrix = np.matrix(filtered_data)
-X, Y = npMatrix[:,0], npMatrix[:,1]
-mdl = LinearRegression().fit(X,Y) # either this or the next line
-#mdl = LinearRegression().fit(filtered_data[['x']],filtered_data.y)
-m = mdl.coef_[0]
-b = mdl.intercept_
+# Linear Regression Object 
+lin_regression = LinearRegression()
 
-# show alternate way to get equation of the line
-print "formula: y = {0}x + {1}".format(m, b) # following slope intercept form 
+# Fitting linear model to the data
+lin_regression.fit(x,y)
 
-# 
-# show how to plot using non python notebooks
-plt.scatter(X,Y, color='blue')
-plt.plot([0,100],[b,m*100+b],'r')
-plt.title('Linear Regression Example', fontsize = 20)
-plt.xlabel('X', fontsize = 15)
-plt.ylabel('Y', fontsize = 15)
+# Get slope of fitted line
+m = lin_regression.coef_
+
+# Get y-Intercept of the Line
+b = lin_regression.intercept_
+
+# Get Predictions for original x values
+# you can also get predictions for new data
+predictions = lin_regression.predict(x)
+
+# following slope intercept form 
+print "formula: y = {0}x + {1}".format(m, b) 
+
+# Plot the Original Model (Black) and Predictions (Blue)
+plt.scatter(x, y,  color='black')
+plt.plot(x, predictions, color='blue',linewidth=3)
+plt.show()
