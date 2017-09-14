5bba5cf81bf8edc10f748f21697205bebb226b04
mGalarnyk <mgalarny@gmail.com>
2017-09-14 11:52:52 -0700

    Adding base scraper

---
+++ Scrapy/fundrazr/fundrazr/spiders/fundrazr_scrape.py
@@ -0,0 +1,29 @@
+import scrapy
+from fundrazr.items import FundrazrItem
+from datetime import datetime
+import re
+
+
+class Fundrazr(scrapy.Spider):
+	name = "my_scraper"
+
+	allowed_domains = ["https://fundrazr.com/"]
+
+	# Potential Start Urls
+	start_urls = ["https://fundrazr.com/find?category=Health"]
+
+	# response.xpath('//html/body').css('.tile-img-contain').xpath('./a[1]/@href')
+
+	npages = 50
+	for i in range(2, npages +1 ):
+		start_urls.append("https://fundrazr.com/find?category=Health&page="+str(i)+"")
+
+	# getting individual campaigns
+	def parse(self, response):
+		for href in response.xpath('//html/body').css('.tile-img-contain').xpath('./a[1]/@href'):
+			url = response.urljoin(href.extract())
+			yield scrapy.Request(url, callback=self.parse_dir_contents)	
+					
+	def parse_dir_contents(self, response):
+		item = FundrazrItem()
+		yield item
