eaa803cf96f0cb7e43e2d0083d688656621d5980
Michael Galarnyk <mGalarnyk@users.noreply.github.com>
2017-09-18 11:42:55 -0700

    Add files via upload

--- Scrapy/fundrazr/fundrazr/spiders/fundrazr_scrape.py
+++ Scrapy/fundrazr/fundrazr/spiders/fundrazr_scrape.py
@@ -13,7 +13,7 @@ class Fundrazr(scrapy.Spider):
 	npages = 2
 
 	# This mimics getting the pages using the next button. 
-	for i in range(2, npages +1 ):
+	for i in range(2, npages + 2):
 		start_urls.append("https://fundrazr.com/find?category=Health&page="+str(i)+"")
 	
 	def parse(self, response):
